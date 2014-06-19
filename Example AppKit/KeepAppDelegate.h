18ac5d8d6eb5c5343377808918e64e5dffaffac3
Martin Kiss <martin.kiss@me.com>
2014-06-19 23:19:00 +0200

    OSX: Create AppKit example app

---
+++ Example AppKit/KeepAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  KeepAppDelegate.h
+//  Example AppKit
+//
+//  Created by Martin Kiss on 19.6.14.
+//  Copyright (c) 2014 Martin Kiss. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface KeepAppDelegate : NSObject <NSApplicationDelegate>
+
+@property (assign) IBOutlet NSWindow *window;
+
+@end
---
+++ Example AppKit/Supporting Files/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  Example AppKit
+//
+//  Created by Martin Kiss on 19.6.14.
+//  Copyright (c) 2014 Martin Kiss. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[])
+{
+    return NSApplicationMain(argc, argv);
+}
